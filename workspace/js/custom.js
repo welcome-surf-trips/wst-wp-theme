3991813a764ecec3c7764a10ca0ac7d8a263dcdf
saif <good4work.saif@gmail.com>
2015-08-22 14:22:55 +0600

    Update custom.js
    Update

--- workspace/js/custom.js
+++ workspace/js/custom.js
@@ -3,13 +3,13 @@ jQuery(function(){
     jQuery(window).scroll(function (e) {
         var distanceTop = window.pageYOffset || document.documentElement.scrollTop;
         var beginAnimationAt = 50;
-
         if (distanceTop > beginAnimationAt) {
             jQuery(logoIcon).addClass('smaller', 1000);
         } else {
             jQuery(logoIcon).removeClass('smaller');
         }
     });*/
+   
      jQuery(window).scroll(function (e) {  
      var logoIcon = jQuery('.logo, .header3-social');       
         var pscroll = jQuery(window).scrollTop();
@@ -24,4 +24,8 @@ jQuery(function(){
             jQuery(logoIcon).removeClass('smaller');
         }
     });
+       jQuery('.outer-center').on('click',function(){
+        window.location.href = 'http://betablog.welcomesurftrips.com.br/';
+     });
+    
 })
